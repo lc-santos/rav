7be5a331f1d5dfddc7676bba749a0a785da229ce
Lucas Silva Santos <lcsantos.xp@gmail.com>
2026-02-27 21:16:05 -0300

    Delete sair.php

--- sair.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();    // Inicia a sessão
-session_unset();    // Limpa as variáveis da sessão
-session_destroy();  // Destrói a sessão
-header("Location: ../login.php"); // Redireciona para a página de login
-exit();
-?>
